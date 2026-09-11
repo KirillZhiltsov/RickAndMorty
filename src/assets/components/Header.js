003211acf14d66f9e20bc287a92627e6ea262ff7
KirillZhiltsov <rpc70917@gmail.com>
2026-09-11 12:30:05 +0300

    RickAndMorty

---
+++ src/assets/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <perdiction className="perdiction__header">
+            <div className="logo">
+                Rick and Morty
+            </div>
+            <div className="navigation">
+            </div>
+        </perdiction>
+    )
+}
+
+export default Header
