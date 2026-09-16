be2286868a8fe717741f95eabb5dfea6c0798fe1
KirillZhiltsov <rpc70917@gmail.com>
2026-09-16 22:11:59 +0300

    Character Card version 2.0

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,4 +1,15 @@
 const Character = () => {
+    let text = 'Alive'
+    let className1, className2;
+
+    if(text === "Alive"){
+        className1 = 'info_green'
+        className2 = 'info-1-1_green'
+    } else {
+        className1 = 'info_red'
+        className2 = 'info-1-1_red'
+    }
+
     return (
         <div className="character-card">
             <div className="img-character">
@@ -6,9 +17,9 @@ const Character = () => {
             </div>
             <div className="character-info">
                 <h3 className="name">Rick Sanchez</h3>
-                <div className="info">
-                    <div className = "info-1-1"></div>
-                    <span>Alive</span>
+                <div className={className1}>
+                    <div className = {className2}></div>
+                    <span>{text}</span>
                 </div>
                 <div className="info">
                     <div className = "info-2-1"></div>
